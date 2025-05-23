914c04e2df0ab455e1fbe06e5967c5de1db6a4d5
Kristin Ding <haozhd2@uw.edu>
2025-05-22 19:50:13 -0700

    Solver: Reformat Single-depot VRP CP solver and include example data

--- solver/CpSAT.py
+++ solver/CpSAT.py
@@ -7,7 +7,7 @@ class SingleDepotVRP:
     """Class to solve the Single Depot Vehicle Routing Problem (SDVRP) using Google OR-Tools.
 
     This class uses the Constraint Programming (CP) solver from Google OR-Tools to find an optimal
-    solution for the SDVRP. The problem is defined by a depot and a set of orders, each with a specific
+    solution for the SDVRP. The problem is defined by a depot and a set of stops, each with a specific
     weight and volume. The goal is to minimize the total travel time while satisfying the constraints
     of vehicle capacity and order delivery.
     The class allows for setting hyperparameters for the optimization, such as penalties for order
@@ -18,19 +18,17 @@ class SingleDepotVRP:
         depot_data (dict): Contains information about the depot,
         including address and the available vehicle types.
 
-        order_data (list[dict]): Contains information about the orders,
+        stop_data (dict): Contains information about the stops,
         including address, weight, and volume. Every numerical elements need to be
         preprocessed to use the metric system instead of imperial system.
         The first order in the list is considered as the depot. (not a real order)
     """
-    def __init__(self, depot_data: dict, order_data: list[dict]):
+    def __init__(self, depot_data: dict, stop_data: dict):
         self.depot = depot_data
-        self.orders = order_data
-        self.addresses = [depot_data["address"]] + [
-            order["address"] for order in order_data
-        ]
-        self.weights = [0] + [order["weight"] for order in order_data]
-        self.volumes = [0] + [order["volume"] for order in order_data]
+        self.stops = stop_data
+        self.addresses = [depot_data["address"]] + list(self.stops.keys())
+        self.weights = [0] + [stop["weight"] for _, stop in self.stops]
+        self.volumes = [0] + [stop["volume"] for _, stop in self.stops]
         self.vehicles = []
         self.order_waiting_times = []
         self.route_durations = []
@@ -39,7 +37,7 @@ class SingleDepotVRP:
         self.solver = cp_model.CpSolver()
         self.alpha = 0  # Hyperparameter: penalty of order waiting time
         self.beta = 0  # Hyperparameter: penalty for using a vehicle
-        self.stopping_time = 0  # Stopping time at each order
+        self.stopping_time = [0] * len(self.addresses)  # Stopping time at each order
         self.max_duty_time = np.inf  # Maximum delivery time for each schedule
 
     def set_hyperparams(self, *, alpha: float | None = None, beta: float | None = None):
@@ -65,13 +63,13 @@ class SingleDepotVRP:
         pass
 
     def solve(self):
-        num_nodes = len(self.orders)
+        num_nodes = len(self.addresses)
         # Decision variables
         x = {}  # x[i][j][k] = 1 if vehicle k travels from order i to order j directly
         y = {}  # y[j][k] = 1 if vehicle k serves order j
         z = {}  # z[k] = 1 if vehicle k is used
         u = {}  # u[i][k] = arrival time of order i at vehicle k
-        if not self.vehicles or not self.orders:
+        if not self.vehicles or not self.stops:
             raise ValueError("Vehicle or order information is not set.")
         for k, (weight_cap, volume_cap, cruising_dist) in enumerate(self.vehicles):
             z[k] = self.model.NewBoolVar(f"z_{k}")
@@ -121,7 +119,9 @@ class SingleDepotVRP:
                         self.model.Add(
                             u[i, k] + 1 <= u[j, k] + (num_nodes) * (1 - x[i, j, k])
                         )
-
+            # Constraint 5: Maximum duty time
+            # The total travel time and stopover time must not exceed
+            # the maximum duty time of the driver
             travel_time = sum(
                 self.route_durations[i][j] * x[i, j, k]
                 for i in range(num_nodes)
@@ -131,9 +131,6 @@ class SingleDepotVRP:
             stopover_time = sum(
                 self.stopping_time * y[j, k] for j in range(1, num_nodes)
             )
-            # Constraint 5: Maximum duty time
-            # The total travel time and stopover time must not exceed
-            # the maximum duty time of the driver
             self.model.Add(travel_time + stopover_time <= self.max_duty_time)
             # Constraint 6: Maximum distance
             # The total travel distance must not exceed the maximum cruising
@@ -149,7 +146,7 @@ class SingleDepotVRP:
             )
 
         # Constraint 7: Each customer must be served exactly once
-        for j in range(1, len(self.orders)):
+        for j in range(1, num_nodes):
             self.model.Add(sum(y[j, k] for k in range(len(self.vehicles))) == 1)
 
         # Objective: Minimize
@@ -179,5 +176,5 @@ class SingleDepotVRP:
             return self.solver.Value
         raise ValueError("No feasible solution found.")
 
-    def interpret_solution(self, solution):
+    def interpret_solution(self, solution, save_path=None):
         pass
